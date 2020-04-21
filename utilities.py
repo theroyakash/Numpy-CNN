67918dd66d644c9d64e07ac69b07e4989644bc5d
The Roy Akash <theakashaiims@gmail.com>
2020-04-21 10:50:25 +0530

    utility added

---
+++ utilities.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image, 
+    as illustrated in Figure 1.
+    
+    Argument:
+
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+    
+    Returns:
+    
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+    
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=(0, 0))
+    
+    return X_pad
+
+
+'''
+In this part, we implement a single step of convolution, 
+in which we apply the filter to a single position of the input. 
+This will be used to build a convolutional unit, which:
+
+- Takes an input volume
+- Applies a filter at every position of the input
+- Outputs another volume (usually of different size)
+
+'''
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+    of the previous layer.
+    
+    Arguments:
+
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+    
+    Returns:
+
+    Z -- a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+
+    # Element-wise product between a_slice_prev and W. Do not add the bias yet.
+    s = a_slice_prev * W
+    # Sum over all entries of the volume s.
+    Z = np.sum(s)
+    # Add bias b to Z. Cast b to a float() so that Z results in a scalar value.
+    Z = float(Z + b)
+
+    return Z
+
+
